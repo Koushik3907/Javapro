1bf594775efcdf8301fa00bfab6d0571ec12ae0f
Koushik3907 <koushik87888@gmail.com>
2024-01-25 20:53:31 +0530

    Changes are made

--- Koushik/src/Exceptionm/Balance.java
+++ Koushik/src/Exceptionm/Balance.java
@@ -2,16 +2,19 @@ package Exceptionm;
 
 import java.util.Scanner;
 
+
 public class Balance {
 	int balance=10000;
 	int with;
+	int sum=10000;
 	String pass;
+	String pass1;
 	static int emoji  =0x1F600;
 public static void main(String[] args) {
 	Balance b = new Balance();
 	Scanner sc = new Scanner(System.in);
 	for(int i=0;i<=100;i++) {
-	System.out.println("1.Register\n2.Login");
+	System.out.println("1.Register\n2.Login\n3.Balance");
 
 	int option=sc.nextInt();
 	switch(option) {
@@ -23,24 +26,52 @@ public static void main(String[] args) {
 		break;
 	case 2:
 		System.out.println("For Login Enter the password");
-		String pass1=sc.next();
-		if (b.pass!=null && b.pass.equals(pass1)) {
+		 b.pass1=sc.next();
+		if (b.pass!=null && b.pass.equals(b.pass1)) {
 			System.out.println("Login Sucessfulfull");
-		try {
-			b.withdrawal();
-		} catch (LimitExceedException e) {
-	            e.printStackTrace();
+			System.out.println("1.Deposit\n2.Withdrawal");
+			int op=sc.nextInt();
+			switch(op) {
+			case 1:
+				System.out.println("Enter the Amount to be withdrawn");
+				int depo=sc.nextInt();
+			    b.sum=b.balance+depo;
+				System.out.println("deposit Successfull"+b.sum);
+				
+				break;
+			case 2:
+				try {
+					b.withdrawal();
+				} catch (LimitExceedException e) {
+			            e.printStackTrace();
+				}
+				}
+				if (b.pass==null || !b.pass.equals(b.pass1)) {
+					System.out.println("Entered Wrong Password or Password has not Genrated \nplease try again later\n"+Character.toString(emoji));
+					
+				}
+				 break;
+		
+			}
+			break;
+	case 3:
+		System.out.println("Enter the password");
+		b.pass1=sc.next();
+		if (b.pass!=null && b.pass.equals(b.pass1)) {
+		System.out.println("Balance in account"+b.sum);
 		}
-		}
-		if (b.pass==null || !b.pass.equals(pass1)) {
-			System.out.println("Entered Wrong Password or Password has not Genrated \nplease try again later\n"+Character.toString(emoji));
+		if (b.pass==null || !b.pass.equals(b.pass1)) {
+			System.out.println("Entered Wrong Password or Password has not Genrated \nplease try again later\n");
 			
 		}
-		 break;
+		break;
+			}
+			
+			
+		
 		
 	}
-		
-	}
+	
 
 	
 }
@@ -51,7 +82,7 @@ class LimitExceedException extends Exception{
 
 public void withdrawal() throws LimitExceedException{
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Balance Remaing in bank Account:"+balance);
+	System.out.println("Balance Remaing in bank Account:"+sum);
 	System.out.println("Enter the amount to be WithDrawn:");
 	with = sc.nextInt();
 	if (with>balance) {
@@ -60,7 +91,7 @@ public void withdrawal() throws LimitExceedException{
 		
 	} else {
      System.out.println("Withdrawn Successfull");
-     System.out.println("Balance Amount:"+(balance-with));
+     System.out.println("Balance Amount:"+(sum-with));
 	}
 }
 }
